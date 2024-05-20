a0d7ed3945acce1538e4146d9951884ab1665424
Jessica <jessica@ramava.art>
2024-05-20 11:21:37 -0500

    Fixed ability to use the remove button on the clone list. #10

--- MultiBoxHelper/Windows/ConfigWindow.cs
+++ MultiBoxHelper/Windows/ConfigWindow.cs
@@ -1,30 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Numerics;
-using System.Runtime.InteropServices;
-using System.Xml.Linq;
 using Dalamud.Game.ClientState.Objects.SubKinds;
+using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.Interface;
 using Dalamud.Interface.Components;
 using Dalamud.Interface.Utility;
 using Dalamud.Interface.Windowing;
-using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
 using ImGuiNET;
-using Lumina.Excel.GeneratedSheets;
+using System;
+using System.Numerics;
 using World = Lumina.Excel.GeneratedSheets.World;
 
 namespace MultiBoxHelper.Windows;
 
+/// <summary>
+/// Configuration UI
+/// </summary>
 public class ConfigWindow : Window, IDisposable
 {
     //private Configuration configuration;
     private readonly Plugin plugin;
     private Vector2 iconButtonSize = new(16);
 
-    //private string world = string.Empty;
-    //private string clone = string.Empty;
-
-    private string selectedCharacter = string.Empty;
+    // selected entry in clone list
+    private string selectedClone = string.Empty;
     private uint selectedWorld = 0;
 
     // We give this window a constant ID using ###
@@ -40,36 +37,34 @@ public class ConfigWindow : Window, IDisposable
 
     public void Dispose()
     {
+       
     }
 
-
-    public override void PreDraw()
-    {
-        SizeConstraints = new WindowSizeConstraints
-        {
-            MinimumSize = new Vector2(400, 300),
-            MaximumSize = new Vector2(800, 600)
-        };
-    }
-
+    /// <summary>
+    /// Draw the window
+    /// </summary>
     public override void Draw()
     {
         // Character list
         ImGui.BeginGroup();
         {
-            if (ImGui.BeginChild("clone_list", ImGuiHelpers.ScaledVector2(240, 300) - iconButtonSize with { X = 0 }, true))
+            if (ImGui.BeginChild("clone_list", ImGuiHelpers.ScaledVector2(240, 300) - iconButtonSize with
+            {
+                X = 0
+            }, true))
             {
                 DrawCharacterList();
             }
             ImGui.EndChild();
 
             // Need to add some buttons below
+
+            // Add current player character
             if (ImGuiComponents.IconButton(FontAwesomeIcon.User))
             {
-                // Add current player character
-                if (Service.ClientState != null && Service.ClientState.LocalPlayer != null)
+                if (Service.ClientState != null)
                 {
-                    plugin.Configuration.AddClone(Service.ClientState.LocalPlayer);
+                    AddClone(Service.ClientState.LocalPlayer);
                 }
             }
             if (ImGui.IsItemHovered())
@@ -78,13 +73,10 @@ public class ConfigWindow : Window, IDisposable
             }
             ImGui.SameLine();
 
+            // add target character
             if (ImGuiComponents.IconButton(FontAwesomeIcon.DotCircle))
             {
-                // add target character
-                if (Service.Targets.Target is PlayerCharacter pc)
-                {
-                    plugin.Configuration.AddClone(pc);
-                }
+                AddClone(Service.Targets.Target);
             }
             if (ImGui.IsItemHovered())
             {
@@ -94,7 +86,8 @@ public class ConfigWindow : Window, IDisposable
 
             if (ImGuiComponents.IconButton(FontAwesomeIcon.Trash))
             {
-                // TODO: Delete character from list
+                // Delete character from list
+                RemoveClone(selectedWorld, selectedClone);
             }
             if (ImGui.IsItemHovered())
             {
@@ -183,30 +176,29 @@ public class ConfigWindow : Window, IDisposable
         ImGui.Text("Clone List");
         ImGui.PushItemWidth(225);
         ImGui.BeginListBox(string.Empty, new Vector2(225, 235));
-        
+
         foreach (var (worldId, list) in plugin.Configuration.CloneList)
         {
             var world = Service.Data.GetExcelSheet<World>()?.GetRow(worldId);
-            if (world == null) continue;
+            if (world == null)
+                continue;
 
             ImGui.TextDisabled(world.Name.RawString);
             ImGui.Separator();
 
             foreach (var clone in list.ToArray())
             {
-                if (ImGui.Selectable($"{clone}##{world.Name.RawString}", (selectedCharacter == clone && selectedWorld == worldId)))
+                if (ImGui.Selectable($"{clone}##{world.Name.RawString}", (selectedClone == clone && selectedWorld == worldId)))
                 {
-                    selectedCharacter = clone;
+                    selectedClone = clone;
                     selectedWorld = worldId;
                 }
-                
+
                 if (ImGui.BeginPopupContextItem())
                 {
                     if (ImGui.Selectable($"Remove '{clone} @ {world.Name.RawString}'"))
                     {
-                        plugin.Configuration.RemoveClone(worldId, clone);
-                        selectedCharacter = string.Empty;
-                        selectedWorld = 0;
+                        RemoveClone(worldId, clone);
                     }
                     ImGui.EndPopup();
                 }
@@ -214,5 +206,34 @@ public class ConfigWindow : Window, IDisposable
         }
         ImGui.EndListBox();
 
+    }
+
+    /// <summary>
+    /// Remove clone from configuration
+    /// </summary>
+    /// <param name="worldId">world identifier</param>
+    /// <param name="clone">character name</param>
+    private void RemoveClone(uint worldId, string clone)
+    {
+        if (plugin != null)
+        {
+            plugin.Configuration.RemoveClone(worldId, clone);
+            selectedClone = string.Empty;
+            selectedWorld = 0;
+        }
+    }
+
+    /// <summary>
+    /// Add a clone to the list from a game object
+    /// </summary>
+    /// <param name="pc">player character object</param>
+    private void AddClone(GameObject? clone)
+    {
+        if (clone != null && clone is PlayerCharacter pc)
+        {
+            plugin.Configuration.AddClone(pc);
+            selectedClone = pc.Name.TextValue;
+            selectedWorld = pc.HomeWorld.Id;
+        }
     }
 }
